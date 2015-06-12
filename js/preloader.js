94065013a3de9260063f9a10e2d816a6e60fe891
Maeb <vogeltak@gmail.com>
2015-06-12 23:26:33 +0200

    Add & load audio files for main menu

--- js/preloader.js
+++ js/preloader.js
@@ -7,9 +7,22 @@ YINS.Preloader = function(game) {};
 
 YINS.Preloader.prototype = {
 	preload: function() {
-		/* Visualize current state for the user */
-		YINS.game.stage.backgroundColor = YINS.color.purple;
+		// Inform the user of the current loading state
+		this.stage.backgroundColor = YINS.color.purple;
 		var text = YINS.game.add.text(YINS.game.world.centerX, YINS.game.world.centerY, "Loading...", YINS.text.header);
 		text.anchor.set(0.5);
+
+		// Load all assets needed for the game
+		YINS.game.load.audio('menuMusic', ['audio/mainmenu.mp3', 'audio/mainmenu.ogg']);
+	},
+
+	create: function() {
+
+	},
+
+	update: function() {
+		if (YINS.game.cache.isSoundDecoded('menuMusic')) {
+			YINS.game.state.start('mainMenu');
+		}
 	}
 };
