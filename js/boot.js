fd2d5f1b27b5e9074f8e48740f351e6bc2fab6be
Maeb <vogeltak@gmail.com>
2015-06-12 17:10:01 +0200

    Add global vars & add prototypes

--- js/boot.js
+++ js/boot.js
@@ -1,17 +1,41 @@
 /*
  *    boot.js
- *    Sets up the game and adds states
+ *    Sets up game and loads preloader assets
  */
 
 /* Sets a global object to hold all the states of the game */
 YINS = {
 
 	/* declare global variables, these will persist across different states */
-	score: 0
+	score: 0,
+
+	/* Declare global colors used throughout the game 
+	Usage example: YINS.color.purple  */
+	color: {
+		purple: '#673ab7'
+	},
+
+	/* Text styles are available by calling: YINS.text.STYLE 
+	for example: YINS.text.header  */
+	text: {
+		header: {
+			font: '8vh Arial',
+			fill: '#f0f0f0',
+			align: 'center'
+		}
+	}
 
 }
 
 /* Constructor creates the game object */
-YINS.Boot = function() {
-	YINS.game = new Phaser.Game(YINS.gameWidth, YINS.gameHeight, Phaser.CANVAS, 'game');
-}
+YINS.Boot = function(game) {};
+
+YINS.Boot.prototype = {
+	preload: function() {
+		// Load assets for the preloader state
+	},
+
+	create: function() {
+		YINS.game.state.start('preloader');
+	}
+};
