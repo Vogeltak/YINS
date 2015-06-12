8538f1ed5e7a5ebc587c7251ad151aae400ed4f7
Maeb <vogeltak@gmail.com>
2015-06-12 23:28:30 +0200

    Init mainmenu state file

---
+++ js/mainmenu.js
@@ -0,0 +1,22 @@
+/*
+ *    mainmenu.js
+ *    Handles main menu
+ */
+
+YINS.MainMenu = function(game) {
+	this.music = null;
+};
+
+YINS.MainMenu.prototype = {
+	create: function() {
+		/* Our assets are preloaded, so here we just kick things off
+		by playing some music */
+		this.music = YINS.game.add.audio('menuMusic');
+		this.music.loopFull(0.6);
+
+		this.stage.backgroundColor = YINS.color.purple_light;
+
+		var title = YINS.game.add.text(YINS.game.world.centerX, YINS.game.world.centerY, "YINS", YINS.text.title);
+		title.anchor.set(0.5);
+	}
+};
