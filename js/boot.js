12b15cc373b9bf19f75951845c26d2ffb3673013
Maeb <vogeltak@gmail.com>
2015-06-12 23:29:22 +0200

    Add vars & disable auto pausing

--- js/boot.js
+++ js/boot.js
@@ -12,12 +12,19 @@ YINS = {
 	/* Declare global colors used throughout the game 
 	Usage example: YINS.color.purple  */
 	color: {
-		purple: '#673ab7'
+		purple: '#673ab7',
+		purple_light: '#b39ddb'
 	},
 
 	/* Text styles are available by calling: YINS.text.STYLE 
 	for example: YINS.text.header  */
 	text: {
+		title: {
+			font: '12vh Courier New',
+			fill: '#404040',
+			align: 'center'
+		},
+
 		header: {
 			font: '8vh Arial',
 			fill: '#f0f0f0',
@@ -38,6 +45,10 @@ YINS.Boot.prototype = {
 	},
 
 	create: function() {
+		//  Phaser will automatically pause if the browser tab the game is in loses focus.
+		//  This disables that
+        YINS.game.stage.disableVisibilityChange = true;
+
 		YINS.game.state.start('preloader');
 	}
 };
