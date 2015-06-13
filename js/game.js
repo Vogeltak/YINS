17d277f2177053259f0f898657e19df8c2a2fb8f
Maeb <vogeltak@gmail.com>
2015-06-13 11:06:10 +0200

    Loop game music

--- js/game.js
+++ js/game.js
@@ -4,13 +4,17 @@
  */
 
 YINS.Game = function(game) {
-
+	this.music;
 };
 
 YINS.Game.prototype = {
 
 	create: function() {
-		
+
+		/* TODO: transition smoothly from mainmenu music to this */
+		this.music = YINS.game.add.audio('gameMusic');
+		this.music.loopFull(0.5);
+
 	},
 
 	update: function() {
